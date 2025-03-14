afdf12b79d93249e048304c45d3d86df3b1e61c6
EzeanoroEbuka <chukwuebukaezeanoro2@gmail.com>
2025-03-14 19:25:30 +0100

    Made updates to the UI

---
+++ components/homes/home-1/Features.jsx
@@ -0,0 +1,248 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Features() {
+  const [activeTab, setActiveTab] = useState(1);
+  return (
+    <div
+      id="main_features"
+      className="main-features section panel overflow-hidden"
+    >
+      <div className="section-outer panel pb-6 xl:pb-9">
+        <div className="container max-w-xl lg:max-w-lg">
+          <div
+            className="section-inner panel"
+            data-anime="onview: -100; targets: >*; translateY: [48, 0]; opacity: [0, 1]; easing: spring(1, 80, 10, 0); duration: 450; delay: anime.stagger(100, {start: 100});"
+          >
+            <div className="panel vstack items-center gap-2 xl:gap-3 mb-4 sm:mb-8 xl:mb-9 sm:max-w-600px xl:max-w-700px mx-auto text-center">
+              <h2 className="h3 lg:h2 xl:h1 m-0">
+                Transform Your WhatsApp Marketing Campaigns
+
+              </h2>
+              <p className="fs-6 xl:fs-5 text-dark dark:text-white text-opacity-70">
+                Elevate your communication game and access powerful features tailored to enhance your business experience.
+              </p>
+            </div>
+            <div className="panel vstack gap-4 xl:gap-6">
+              <div className="panel px-3 rounded-2 bg-secondary dark:bg-gray-800">
+                <ul
+                  className="main-features-nav hstack text-center overflow-auto"
+                  data-uc-switcher="connect: .main-features-switcher; active: 1; animation: uc-animation-fade;"
+                  role="tablist"
+                >
+                  <li
+                    className={`${activeTab == 1 ? "uc-active" : ""}`}
+                    onClick={() => setActiveTab(1)}
+                    role="presentation"
+                  >
+                    <a className="h6 lg:h5 text-nowrap m-0">Flow Builder</a>
+                  </li>
+                  <li
+                    onClick={() => setActiveTab(2)}
+                    className={`${activeTab == 2 ? "uc-active" : ""}`}
+                    role="presentation"
+                  >
+                    <a className="h6 lg:h5 text-nowrap m-0">Automated Responses</a>
+                  </li>
+                  <li
+                    className={`${activeTab == 3 ? "uc-active" : ""}`}
+                    onClick={() => setActiveTab(3)}
+                    role="presentation"
+                  >
+                    <a className="h6 lg:h5 text-nowrap m-0">
+                      Contact Management
+                    </a>
+                  </li>
+                  <li
+                    className={`${activeTab == 4 ? "uc-active" : ""}`}
+                    onClick={() => setActiveTab(4)}
+                    role="presentation"
+                  >
+                    <a className="h6 lg:h5 text-nowrap m-0">Multiple Users</a>
+                  </li>
+                </ul>
+              </div>
+              <div
+                className="main-features-switcher uc-switcher overflow-hidden"
+                role="presentation"
+                style={{ touchAction: "pan-y pinch-zoom" }}
+              >
+                {activeTab == 1 && (
+                  <div className="uc-active">
+                    <div className="feature-item panel">
+                      <div className="row child-cols col-match justify-between g-4">
+                        <div className="col-12 sm:col-6">
+                          <figure className="featured-image m-0 rounded ratio ratio-1x1 rounded-2 overflow-hidden">
+                            <Image
+                              className="media-cover image"
+                              alt="Share tools quickly and confidently in minutes"
+                              src="/assets/images/template/feature-image-02.jpg"
+                              width="1200"
+                              height="1200"
+                            />
+                          </figure>
+                        </div>
+                        <div className="col-12 sm:col-6">
+                          <div className="panel">
+                            <div className="panel vstack justify-center gap-4 h-100 sm:p-3 lg:p-4">
+                              <div>
+                                <div className="panel vstack gap-2">
+                                  <h3 className="h4 sm:h3 lg:h2 m-0">
+                                    Design and Automate WhatsApp Conversations with Ease
+                                  </h3>
+                                  <p className="fs-6 lg:fs-5 opacity-70 dark:opacity-80">
+                                    Create seamless, interactive messaging flows using a drag-and-drop interface—no coding required!
+                                  </p>
+                                 <Link
+                                    href="/page-features"
+                                    className="uc-link fw-bold hstack gap-narrow mt-2 sm:mt-1 lg:mt-2"
+                                  >
+                                    <span>Learn More</span>
+                                    <i className="position-relative icon icon-1 unicon-arrow-right rtl:rotate-180 translate-y-px" />
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {activeTab == 2 && (
+                  <div className="uc-active">
+                    <div className="feature-item panel">
+                      <div className="row child-cols col-match justify-between g-4">
+                        <div className="col-12 sm:col-6">
+                          <figure className="featured-image m-0 rounded ratio ratio-1x1 rounded-2 overflow-hidden">
+                            <Image
+                              className="media-cover image"
+                              alt="Connect every part of your entire business"
+                              src="/assets/images/template/feature-image-03.jpg"
+                              width="1200"
+                              height="1200"
+                            />
+                          </figure>
+                        </div>
+                        <div className="col-12 sm:col-6">
+                          <div className="panel">
+                            <div className="panel vstack justify-center gap-4 h-100 sm:p-3 lg:p-4">
+                              <div>
+                                <div className="panel vstack gap-2">
+                                  <h3 className="h4 sm:h3 lg:h2 m-0">
+                                    Instant, Smart Replies to Engage Your Customers 24/7
+                                  </h3>
+                                  <p className="fs-6 lg:fs-5 opacity-70 dark:opacity-80">
+                                    Never miss a message! Craft responses tailored to keep your customers engaged even when you're offline.
+                                  </p>
+                                  <a
+                                    href="#"
+                                    className="uc-link fw-bold hstack gap-narrow mt-2 sm:mt-1 lg:mt-2"
+                                  >
+                                    <span>Learn More</span>
+                                    <i className="position-relative icon icon-1 unicon-arrow-right rtl:rotate-180 translate-y-px" />
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {activeTab == 3 && (
+                  <div className="uc-active">
+                    <div className="feature-item panel">
+                      <div className="row child-cols col-match justify-between g-4">
+                        <div className="col-12 sm:col-6">
+                          <figure className="featured-image m-0 rounded ratio ratio-1x1 rounded-2 overflow-hidden">
+                            <Image
+                              className="media-cover image"
+                              alt="Maintain compliance and control your apps"
+                              src="/assets/images/template/feature-image-01.jpg"
+                              width="1200"
+                              height="1200"
+                            />
+                          </figure>
+                        </div>
+                        <div className="col-12 sm:col-6">
+                          <div className="panel">
+                            <div className="panel vstack justify-center gap-4 h-100 sm:p-3 lg:p-4">
+                              <div>
+                                <div className="panel vstack gap-2">
+                                  <h3 className="h4 sm:h3 lg:h2 m-0">
+                                    Organize, Segment, and Personalize Customer Interactions
+
+                                  </h3>
+                                  <p className="fs-6 lg:fs-5 opacity-70 dark:opacity-80">
+                                    Manage contacts with tags, filters, and custom attributes. Track conversations, segment audiences, and send personalized messages to boost engagement.
+                                  </p>
+                                <Link
+                                    href="/page-features"
+                                    className="uc-link fw-bold hstack gap-narrow mt-2 sm:mt-1 lg:mt-2"
+                                  >
+                                    <span>Learn More</span>
+                                    <i className="position-relative icon icon-1 unicon-arrow-right rtl:rotate-180 translate-y-px" />
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {activeTab == 4 && (
+                  <div className="uc-active">
+                    <div className="feature-item panel">
+                      <div className="row child-cols col-match justify-between g-4">
+                        <div className="col-12 sm:col-6">
+                          <figure className="featured-image m-0 rounded ratio ratio-1x1 rounded-2 overflow-hidden">
+                            <Image
+                              className="media-cover image"
+                              alt="Review quickly and confidently"
+                              src="/assets/images/template/feature-image-04.jpg"
+                              width="1200"
+                              height="1200"
+                            />
+                          </figure>
+                        </div>
+                        <div className="col-12 sm:col-6">
+                          <div className="panel">
+                            <div className="panel vstack justify-center gap-4 h-100 sm:p-3 lg:p-4">
+                              <div>
+                                <div className="panel vstack gap-2">
+                                  <h3 className="h4 sm:h3 lg:h2 m-0">
+                                    Collaborate Seamlessly with Your Team on WhatsApp
+                                  </h3>
+                                  <p className="fs-6 lg:fs-5 opacity-70 dark:opacity-80">
+                                    Enable your team members to access and manage customer conversations from a single platform. Assign roles, track interactions, and ensure consistent messaging across all touchpoints.
+                                  </p>
+                                  <Link
+                                    href="/page-features"
+                                    className="uc-link fw-bold hstack gap-narrow mt-2 sm:mt-1 lg:mt-2"
+                                  >
+                                    <span>Learn More</span>
+                                    <i className="position-relative icon icon-1 unicon-arrow-right rtl:rotate-180 translate-y-px" />
+                                  </Link>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
